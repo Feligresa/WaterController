a90992ee01f9912a44aa763a5d935bc1ec0c5e1c
Feligresa <84331186+Feligresa@users.noreply.github.com>
2026-09-08 15:41:22 +0200

    Semantics, GPIO is not a constant

--- classes/water_controller.py
+++ classes/water_controller.py
@@ -1,11 +1,11 @@
 import time
 
 try:
-    import RPi.GPIO as GPIO
+    import RPi.GPIO as gpio # type: ignore
 except RuntimeError:
     print("Error importing RPi.GPIO. Fallback to test.")
     from .test_gpio import TestGPIO
-    GPIO = TestGPIO()
+    gpio = TestGPIO()
 
 from ..main import PUMP_GPIO, WATERING_DUR_SECONDS
 from ..dtos.sensor_data import SensorData
@@ -13,7 +13,7 @@ from ..dtos.sensor_data import SensorData
 
 class WaterController:
     def water(self, sensor_data: SensorData):
-        GPIO.setmode(GPIO.BOARD)
+        gpio.setmode(gpio.BOARD)
 
         sensors = [sensor_data.sensor1,
                    sensor_data.sensor2,
@@ -37,16 +37,16 @@ class WaterController:
         for valve in openvalves:
             self.toggle_valve(valve)
         
-        GPIO.cleanup()
+        gpio.cleanup()
 
     def toggle_valve(self, valve: int):
-        if GPIO.output(valve, False):
-            GPIO.output(valve, True)
+        if gpio.output(valve, False):
+            gpio.output(valve, True)
         else:
-            GPIO.output(valve, False)
+            gpio.output(valve, False)
 
     def toggle_pump(self):
-        if GPIO.output(PUMP_GPIO, False):
-            GPIO.output(PUMP_GPIO, True)
+        if gpio.output(PUMP_GPIO, False):
+            gpio.output(PUMP_GPIO, True)
         else:
-            GPIO.output(PUMP_GPIO, False)
+            gpio.output(PUMP_GPIO, False)
