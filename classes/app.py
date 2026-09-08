102dd2f7deb9f325e16b994af09eb1a702f28741
Feligresa <84331186+Feligresa@users.noreply.github.com>
2026-09-08 15:38:56 +0200

    Start loop

--- classes/app.py
+++ classes/app.py
@@ -1,8 +1,17 @@
-from ..dtos.sensor_data import SensorData
+import time
+from ..main import INTERVAL_SECONDS
+from .water_controller import WaterController
+from .fetcher import Fetcher
 
 class App:
     def __init__(self):
-        pass
+        self.fetcher = Fetcher()
+        self.water_controller = WaterController()
 
     def start(self):
-        pass
+        while True:
+            sensor_data = self.fetcher.get_latest_sensors()
+
+            self.water_controller.water(sensor_data)
+
+            time.sleep(INTERVAL_SECONDS)
